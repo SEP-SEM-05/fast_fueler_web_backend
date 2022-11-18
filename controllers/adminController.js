e7efa28314ab98e747c6b062b8e05067b30b7413
Sakeerthan Thijakarasa <sakeerkeer13@gmail.com>
2022-11-18 10:00:46 +0530

    send email updated with new url

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -470,6 +470,7 @@ const get_type_org_vehicles = async (req, res) => {
 //send email to station
 const send_email = async (req, res) => {
     let regNo = req.body.regNo;
+    let station = req.body;
 
     let password = generator.generate({
         length: 10,
@@ -478,9 +479,9 @@ const send_email = async (req, res) => {
 
     const msg = {
         from: "fastfueler001@gmail.com",
-        to: req.body.email,
+        to: station.email,
         subject: "Welcome to Fast Fueler",
-        text: "Now you can go to this link (http://localhost:3000/fuelstationgetstands/"+regNo+") and login to our system using this temporary password \nTemp password: " + password
+        text: "Welcome to Fast Fueler! Now you can join with us using this link (https://fast-fueler-frontend.firebaseapp.com/fuelstationgetstands/"+regNo+") and login to your account using this temporary password \nTemp password: " + password+"\n\nThank you!"
     };
 
     try{
@@ -537,7 +538,7 @@ const send_email_to_all = async (req, res) => {
                 from: "fastfueler001@gmail.com",
                 to: station.email,
                 subject: "Welcome to Fast Fueler",
-                text: "Now you can go to this link (http://localhost:3000/fuelstationgetstands/"+regNo+") and login to our system using this temporary password \nTemp password: " + password
+                text: "Welcome to Fast Fueler! Now you can join with us using this link (https://fast-fueler-frontend.firebaseapp.com/fuelstationgetstands/"+regNo+") and login to your account using this temporary password \nTemp password: " + password+"\n\nThank you!"
             };
     
             nodemailer.createTransport({
@@ -612,7 +613,6 @@ const get_count_vehicle = async (req, res) => {
         });
     }
 }
-
 
 module.exports = {
     get_dashboard,
